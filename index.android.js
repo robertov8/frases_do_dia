2720a08267127729be68f20b2691a536f4ffd792
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-02 19:36:40 -0300

    Section 7, class 83

--- index.android.js
+++ index.android.js
@@ -11,7 +11,9 @@ export default class frases_do_dia extends Component {
   render() {
     return (
         <View style={styles.estiloView}>
-            <Text style={styles.estiloTexto}>Frases do dia</Text>
+            <Text style={styles.estiloTexto}>A</Text>
+            <Text style={styles.estiloTexto2}>B</Text>
+            <Text style={styles.estiloTexto}>C</Text>
         </View>
     );
   }
@@ -21,12 +23,21 @@ const styles = {
     estiloTexto: {
         fontSize: 30,
         backgroundColor: '#08509B',
+        height: 60,
+        width: 60
+    },
+    estiloTexto2: {
+        fontSize: 30,
+        backgroundColor: '#2A48FA',
+        height: 60,
+        width: 60
     },
     estiloView: {
         backgroundColor: 'skyblue',
         height: 300,
-        justifyContent: 'center',
-        alignItems: 'center'
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'column'
     }
 };
 

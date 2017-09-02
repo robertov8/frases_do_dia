2d328bff950ef091fd955959c6321386efe7f8a2
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-02 19:56:41 -0300

    Section 7, class 85

--- index.android.js
+++ index.android.js
@@ -5,36 +5,31 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, View, StyleSheet, Text } from 'react-native';
+import { AppRegistry, View, StyleSheet, Text, Button } from 'react-native';
 
 export default class frases_do_dia extends Component {
-  render() {
-    return (
-        <View style={styles.principal}>
-            <Text style={styles.topo}>Topo</Text>
-            <Text style={styles.conteudo}>Conteudo</Text>
-            <Text style={styles.rodape}>Rodape</Text>
-        </View>
-    );
-  }
+
+    botaoPressionado() {
+        alert('Botao pressionado!');
+    }
+
+    render() {
+        return (
+            <View style={styles.principal}>
+                <Button
+                    title='Clique aqui'
+                    color="#841584"
+                    onPress={this.botaoPressionado}
+                    accessibilityLabel="Clique para abrir as notícias!!!"
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     principal: {
         flex: 1,
-        backgroundColor: 'cornflowerblue'
-    },
-    topo: {
-        flex: 2,
-        backgroundColor: 'brown'
-    },
-    conteudo: {
-        flex: 8,
-        backgroundColor: 'yellowgreen'
-    },
-    rodape: {
-        flex: 1,
-        backgroundColor: 'orangered'
     }
 });
 

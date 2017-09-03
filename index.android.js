8b42a8350a80259b77f54fdbeca9f51f4804c19f
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-02 21:24:54 -0300

    Section 7, class 91

--- index.android.js
+++ index.android.js
@@ -11,19 +11,34 @@ import {
     StyleSheet,
     Text,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    Alert
 } from 'react-native';
 
 export default class frases_do_dia extends Component {
 
     botaoPressionado() {
+        let numberoAleatorio = Math.floor(Math.random() * 5);
+
+        let frases = [
+            'Estou sempre alegre. Essa é a melhor maneira de resolver os problemas da vida.',
+            'O tempo dura bastante para aqueles que sabem aproveitá-lo.',
+            'Se você encontrar um caminho sem obstáculos, ele provavelmente não leva a lugar nenhum.',
+            'Não existe um caminho para a felicidade. A felicidade é o caminho.',
+            'Você nunca sabe a força que tem, até que a sua única alternativa é ser forte.'
+        ];
+
+        let fraseEscolhida = frases[ numberoAleatorio ];
+        Alert.alert('Frase do dia', fraseEscolhida);
     }
 
     render() {
         return (
             <View style={styles.principal}>
                 <Image source={ require('./imgs/logo.png')}/>
-                <TouchableOpacity style={styles.botao}>
+                <TouchableOpacity
+                    onPress={this.botaoPressionado}
+                    style={styles.botao}>
                     <Text style={styles.textoBotao}>Nova frase</Text>
                 </TouchableOpacity>
             </View>
